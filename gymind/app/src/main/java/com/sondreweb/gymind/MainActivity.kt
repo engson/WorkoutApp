672504eaffd4ea66f6fe4857492769e1ab73c847
corast <corastweb94@hotmail.com>
2021-10-30 23:18:05 +0200

    Change target sdk to 30 and added spash screen

--- gymind/app/src/main/java/com/sondreweb/gymind/MainActivity.kt
+++ gymind/app/src/main/java/com/sondreweb/gymind/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.sondreweb.gymind
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.sondreweb.gymind.ui.theme.GyMindTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +32,55 @@ class MainActivity : ComponentActivity() {
         setContent {
             GyMindTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()) {
+                    com.sondreweb.gymind.Navigation()
+                    //Greeting("Android")
                 }
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController,
+        startDestination = "spash_screen") {
+        composable("spash_screen") {
+            SplashScreen(navController = navController)
+        }
+
+        composable("main_screen"){
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController){
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true){
+        scale.animateTo(
+            targetValue = 0.7f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(4f)
+                        .getInterpolation(it)
+                }))
+        delay(2500L)
+        navController.navigate("main_screen")
+    }
+
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()) {
+        Image(painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value))
     }
 }
 
@@ -29,7 +89,7 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(name= "test ", showBackground = true, backgroundColor = 0xffd43f48)
+@Preview(name= "test ", showBackground = true)
 @Composable
 fun DefaultPreview() {
     GyMindTheme {
