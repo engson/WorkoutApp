a10f0583b458e405870a838a12a36e7ff8f31c55
corast <corastweb94@hotmail.com>
2021-10-31 12:30:57 +0100

    Init nav + added drawables

--- gymind/app/src/main/java/com/sondreweb/gymind/MainActivity.kt
+++ gymind/app/src/main/java/com/sondreweb/gymind/MainActivity.kt
@@ -1,27 +1,33 @@
 package com.sondreweb.gymind
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -33,10 +39,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GyMindTheme {
+            GyMindTheme(darkTheme = true) {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()) {
-                    com.sondreweb.gymind.Navigation()
+                    Navigation()
                     //Greeting("Android")
                 }
             }
@@ -44,13 +50,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+@Composable
+fun TopBar() {
+    TopAppBar (
+        title = { Text(text = stringResource(R.string.app_name), fontSize = 18.sp) },
+        backgroundColor = MaterialTheme.colors.secondaryVariant,
+        contentColor = MaterialTheme.colors.primary,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview(){
+    GyMindTheme(darkTheme = true) {
+        TopBar()
+    }
+}
+
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
     NavHost(navController = navController,
-        startDestination = "spash_screen") {
-        composable("spash_screen") {
+        startDestination = "splash_screen") {
+        composable("splash_screen") {
             SplashScreen(navController = navController)
         }
 
@@ -91,14 +113,14 @@ fun SplashScreen(navController: NavController){
 @Composable
 fun Greeting(name: String) {
     Box(modifier = Modifier.fillMaxSize()){
-        Text(text = "Hello $name!", Modifier.padding(16.dp))
+        Text(text = "Hello $name!", color = MaterialTheme.colors.secondary, modifier = Modifier.padding(16.dp))
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi  = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun DefaultPreview() {
-    GyMindTheme(darkTheme = false) {
+    GyMindTheme {
         Greeting("preview")
     }
 }
